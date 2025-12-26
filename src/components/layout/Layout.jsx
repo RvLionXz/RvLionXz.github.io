ef94a6ffb20887e631bcf2778381dca9fcbd9d38
RvLionXz <rvlionxz@gmail.com>
2025-12-27 00:37:37 +0700

    Test production

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,8 +7,6 @@ const Layout = ({ children }) => {
 			<div className="relative min-h-screen w-full bg-background font-sans text-white selection:bg-primary selection:text-black">
 				<Navbar />
 
-				<div className="pointer-events-none fixed inset-0 z-40 h-full w-full bg-[url('/noise.png')] opacity-20 mix-blend-overlay"></div>
-
 				<main className="relative z-10 flex flex-col items-center px-4 md:px-0">
 					{children}
 				</main>
